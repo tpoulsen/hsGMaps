40274e4ea13b638328f945839b37f9726c0193b7
Travis Poulsen <travis.poulsen@gmail.com>
2014-09-23 20:16:49 -0400

    generics

--- haskellMaps.hs
+++ haskellMaps.hs
@@ -7,6 +7,7 @@ import Control.Applicative ((<$>), (<*>))
 import Control.Monad       (mzero)
 import Data.Aeson
 import Data.Monoid
+import GHC.Generics
 import qualified Data.ByteString.Lazy.Internal as BS
 import qualified Data.Text as T
 
@@ -24,23 +25,25 @@ instance FromJSON Ingredient where
   parseJSON _          = mzero 
 -}
 data GeocodeResponse = GeocodeResponse
-    { results :: GeocodeResults
+    { results :: [GeocodeResults]
     , status  :: T.Text
-    } deriving (Show, Eq)
-instance FromJSON GeocodeResponse where
+    } deriving (Show, Eq, Generic)
+instance FromJSON GeocodeResponse 
+{-where
     parseJSON (Object o) =
         GeocodeResponse <$>
                 o .: "results" <*>
                 o .: "status"
     parseJSON _  = mzero
-
+-}
 data GeocodeResults = GeocodeResults
     { addressComponents :: [AddressComponents]
     , formattedAddress  :: T.Text
     , geometry           :: [Coordinates]
     , rTypes              :: [T.Text]
-    } deriving (Show, Eq)
-instance FromJSON GeocodeResults where
+    } deriving (Show, Eq, Generic)
+instance FromJSON GeocodeResults 
+{-where
     parseJSON (Object o) =
         GeocodeResults <$>
                 o .: "address_components" <*>
@@ -48,51 +51,56 @@ instance FromJSON GeocodeResults where
                 o .: "geometry"           <*>
                 o .: "types"
     parseJSON _ = mzero
-
+-}
 data AddressComponents = AddressComponents
     { longName  :: T.Text
     , shortName :: T.Text
     , aTypes      :: [T.Text]
-    } deriving (Show, Eq)
-instance FromJSON AddressComponents where
+    } deriving (Show, Eq, Generic)
+instance FromJSON AddressComponents 
+{-where
     parseJSON (Object o) =
         AddressComponents <$>
                 o .: "long_name"  <*>
                 o .: "short_name" <*>
                 o .: "types"
     parseJSON _ = mzero
-
+-}
 data Geometry = Geometry
     { location     :: Location
     , locationType :: T.Text
     , viewport     :: Viewport
-    } deriving (Show, Eq)
-instance FromJSON Geometry where
+    } deriving (Show, Eq, Generic)
+instance FromJSON Geometry 
+{-where
     parseJSON (Object o) = 
         Geometry <$>
                 o .: "location"      <*>
                 o .: "location_type" <*>
                 o .: "viewport"
     parseJSON _ = mzero
-
+-}
 data Location = Location 
     { lat :: T.Text
     , lng :: T.Text
-    } deriving (Show, Eq)
-instance FromJSON Location where
+    } deriving (Show, Eq, Generic)
+instance FromJSON Location 
+{-where
     parseJSON (Object o) =
         Location <$>
                 o .: "lat" <*>
                 o .: "lng"
     parseJSON _ = mzero 
-
+-}
 data Viewport = Viewport
     { northeast :: Location
     , southwest :: Location
-    } deriving (Show, Eq)
-instance FromJSON Viewport where
+    } deriving (Show, Eq, Generic)
+instance FromJSON Viewport 
+{-where
     parseJSON (Object o) =
         Viewport <$>
                 o .: "northeast" <*>
                 o .: "southwest"
     parseJSON _ = mzero 
+-}
